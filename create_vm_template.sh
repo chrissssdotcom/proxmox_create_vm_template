b49168af3c6f7906e6aeadc749a3c6974833e859
Christian Clark <120564853+chrissssdotcom@users.noreply.github.com>
2024-09-08 13:41:52 +0930

    commit

--- create_vm_template.sh
+++ create_vm_template.sh
@@ -1,79 +1,94 @@
 #!/bin/bash
 
-# Define variables
-MEM=2048              # Amount of RAM in MB
-NET_BRIDGE="vmbr0"    # Network bridge, e.g., vmbr0
-DISK_STOR="local" # Disk storage, e.g., local-lvm
-CPU_CORES=2           # Number of CPU cores
-DISK_SIZE="32G"       # Disk size
-CLOUD_DIR="/var/lib/vz/cloudready"  # Directory to store images
+# Variables
+MEMORY=2048
+CORES=2
+BRIDGE="vmbr0"
+STORAGE="local"  # Change to your storage choice
+DISK_SIZE="16G"  # Desired disk size after expansion
 
-# List of image URLs to download
-IMAGES_URLS=(
-  "https://mirror.aarnet.edu.au/pub/rocky/9/images/x86_64/Rocky-9-GenericCloud.latest.x86_64.qcow2"
-  "https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-generic-amd64.qcow2"
-  "https://cloud-images.ubuntu.com/releases/jammy/release/ubuntu-22.04-server-cloudimg-amd64.img"
-  # Add more images here
+# Array of VM details: (VM_ID, VM_NAME, IMG_URL)
+VM_DETAILS=(
+    "10010 rockylinux-9.4 https://mirror.aarnet.edu.au/pub/rocky/9.4/images/x86_64/Rocky-9-GenericCloud-Base-9.4-20240509.0.x86_64.qcow2"
+    "10020 debian-12 https://cloud.debian.org/images/cloud/bookworm/20240901-1857/debian-12-generic-amd64-20240901-1857.qcow2"
+    "10030 ubuntu-24.10 https://cloud-images.ubuntu.com/oracular/current/oracular-server-cloudimg-amd64.img"
 )
 
-# Initialize unique VMID
-START_VMID=10000
+# Function to create and configure VM
+create_vm() {
+    VM_ID=$1
+    VM_NAME=$2
+    IMG_URL=$3
+    IMG_FILE="${VM_NAME}.qcow2"
 
-# Download images if they do not exist
-mkdir -p "$CLOUD_DIR"
+    echo "---------------------------------"
+    echo "Processing VM: $VM_NAME (ID: $VM_ID)"
+    echo "---------------------------------"
 
-for IMG_URL in "${IMAGES_URLS[@]}"; do
-  IMG_PATH="$CLOUD_DIR/$(basename $IMG_URL)"
-  if [ ! -f "$IMG_PATH" ]; then
-    wget -P "$CLOUD_DIR" "$IMG_URL"
-  else
-    echo "File $IMG_PATH already exists, skipping download."
-  fi
-done
+    # Step 1: Download Ubuntu Cloud Image
+    echo "Downloading Ubuntu Cloud Image for $VM_NAME..."
+    wget $IMG_URL -O $IMG_FILE
 
-# Function to create a VM template
-create_template() {
-  local img_name="$1"
-  local vmid="$2"
+    # Step 2: Resize the disk image to 16GB
+    echo "Resizing disk image for $VM_NAME to $DISK_SIZE..."
+    qemu-img resize $IMG_FILE $DISK_SIZE
 
-  # Generate template name based on the first two segments of the image file name
-  local templ_name=$(basename "$img_name" .qcow2)
-  templ_name=$(basename "$templ_name" .img)  # Handle both .img and .qcow2
-  templ_name=$(echo "$templ_name" | cut -d'-' -f1-2)
+    # Step 3: Create a new virtual machine
+    echo "Creating VM $VM_NAME..."
+    qm create $VM_ID --memory $MEMORY --core $CORES --name $VM_NAME --net0 virtio,bridge=$BRIDGE
 
-  # Append '-template' to the template name
-  templ_name="${templ_name}-template"
+    # Step 4: Import the downloaded disk to local storage
+    echo "Importing disk for $VM_NAME to storage..."
+    qm disk import $VM_ID $IMG_FILE $STORAGE
 
-  # Remove existing VM if a template with the same name already exists
-  if qm list | grep -qw "$templ_name"; then
-    local existing_vmid=$(qm list | grep -w "$templ_name" | awk '{print $1}')
-    echo "Template with name $templ_name already exists with VMID $existing_vmid. Removing..."
-    qm destroy "$existing_vmid" --purge
-  fi
+    # Step 5: Attach the disk as a SCSI drive on the SCSI controller
+    echo "Attaching disk for $VM_NAME as SCSI drive..."
+    qm set $VM_ID --scsihw virtio-scsi-pci --scsi0 $STORAGE:vm-$VM_ID-disk-0
 
-  # Create a new virtual machine
-  virt-customize -a "$img_name" --install qemu-guest-agent
-  qm create "$vmid" --name "$templ_name" --memory $MEM --net0 virtio,bridge=$NET_BRIDGE
-  qm importdisk "$vmid" "$img_name" $DISK_STOR
-  qm set "$vmid" --ide1 $DISK_STOR:vm-$vmid-disk-0
-  qm set "$vmid" --ide2 $DISK_STOR:cloudinit
-  qm set "$vmid" --boot c --bootdisk scsi0
-  qm set "$vmid" --ipconfig0 ip=dhcp
-  qm set "$vmid" --cores $CPU_CORES
-  qm resize "$vmid" scsi0 $DISK_SIZE
-  qm template "$vmid"
-  echo "Virtual machine template $templ_name has been created with VMID $vmid."
+    # Step 6: Add cloud-init drive
+    echo "Adding cloud-init drive for $VM_NAME..."
+    qm set $VM_ID --ide2 $STORAGE:cloudinit
+
+    # Step 7: Make cloud-init drive bootable and restrict BIOS to boot from disk only
+    echo "Setting boot options for $VM_NAME..."
+    qm set $VM_ID --boot c --bootdisk scsi0
+
+    # Step 8: Add serial console
+    echo "Adding serial console for $VM_NAME..."
+    qm set $VM_ID --serial0 socket --vga serial0
+
+    # DO NOT START VM (automatically avoided in script)
+
+    # Optional: Install necessary tools and guest agent before importing the image
+    echo "Installing libguestfs-tools..."
+    apt install -y libguestfs-tools
+
+    echo "Installing qemu-guest-agent on $VM_NAME image..."
+    virt-customize -a $IMG_FILE --install qemu-guest-agent
+
+    # Enable agent in VM settings
+    echo "Enabling agent in VM settings for $VM_NAME..."
+    qm set $VM_ID --agent 1,fstrim_cloned_disks=1
+
+    # Clear machine-id in the image to allow regeneration on first boot
+    echo "Clearing machine ID in $VM_NAME image..."
+    virt-customize -a $IMG_FILE --run-command "sudo truncate -s 0 /etc/machine-id /var/lib/dbus/machine-id"
+
+    # Step 9: Create VM template
+    echo "Creating VM template for $VM_NAME..."
+    qm template $VM_ID
+
+    echo "VM $VM_NAME (ID: $VM_ID) is ready with expanded disk!"
+    echo "---------------------------------"
 }
 
-# Create templates for all downloaded images
-for IMG_URL in "${IMAGES_URLS[@]}"; do
-  IMG_PATH="$CLOUD_DIR/$(basename $IMG_URL)"
-  create_template "$IMG_PATH" "$START_VMID"
-  START_VMID=$((START_VMID + 1)) # Increment VMID for the next machine
-done
-
-# Remove downloaded images after creating templates
-for IMG_URL in "${IMAGES_URLS[@]}"; do
-  IMG_PATH="$CLOUD_DIR/$(basename $IMG_URL)"
-  rm -f "$IMG_PATH"
+# Iterate over each VM entry in the array
+for VM in "${VM_DETAILS[@]}"; do
+    # Split the VM entry into separate variables
+    VM_ID=$(echo $VM | awk '{print $1}')
+    VM_NAME=$(echo $VM | awk '{print $2}')
+    IMG_URL=$(echo $VM | awk '{print $3}')
+    
+    # Call the function to create the VM
+    create_vm $VM_ID $VM_NAME $IMG_URL
 done
